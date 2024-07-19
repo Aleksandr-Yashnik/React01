3a7df0becb99efa78761ea1b0ed9b7e92d29ab93
Aleksandr-Yashnik <jashnik.aleksandr@gmail.com>
2024-07-19 16:16:54 +0200

    сохранение

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import GlobalStyles from "styles/GlobalStyles";
-import Layout from "components/Layout/Layout";
+//import Layout from "components/Layout/Layout";
 import Home from "pages/EmployeeApp/Home/Home";
 import About from "pages/EmployeeApp/About/About";
-import LogIn from "pages/EmployeeApp/LogIn/LogIn";
+//import LogIn from "pages/EmployeeApp/LogIn/LogIn";
 import ContactUs from "pages/EmployeeApp/ContactUs/ContactUs";
 import Clients from "pages/EmployeeApp/Clients/Clients";
 import Apple from "pages/EmployeeApp/Clients/Apple/Apple";
 import Google from "pages/EmployeeApp/Clients/Google/Google";
-import Facebook from "pages/EmployeeApp/Clients/Facebook/Facebook";
+//import Facebook from "pages/EmployeeApp/Clients/Facebook/Facebook";
 
 // Lessons
 import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
 import Lesson_07 from "./lessons/Lesson_07/Lesson_07";
-import Lesson_08 from "lessons/Lesson_08/Lesson_08";
+//import Lesson_08 from "lessons/Lesson_08/Lesson_08";
 import Lesson_09 from "lessons/Lesson_09/Lesson_09";
-import Lesson_11 from "lessons/Lesson_11/Lesson_11";
+//import Lesson_11 from "lessons/Lesson_11/Lesson_11";
 import Lesson_13 from "lessons/Lesson_13/Lesson_13";
 
 // Homeworks
 import Homework_06 from "./homeworks/Homework_06/Homework_06";
-import Homework_07 from "homeworks/Homework_07/Homewotk_07";
+//import Homework_07 from "homeworks/Homework_07/Homewotk_07";
 import Homework_08 from "homeworks/Homework_08/Homework_08";
 import Homework_09 from "homeworks/Homework_09/Homework_09";
-import Homework_11 from "homeworks/Homework_11/Homework_11";
+//import Homework_11 from "homeworks/Homework_11/Homework_11";
 
 // Consultations
-import Consultation_03 from "consultations/Consultation_03/Consultation_03";
+//import Consultation_03 from "consultations/Consultation_03/Consultation_03";
 
 function App() {
   return (
